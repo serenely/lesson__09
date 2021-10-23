1b9ed1a3093be2811c88aaae89f43d97a87d0fb4
Serenely <sgilovboris12@gmail.com>
2021-10-23 20:36:34 +0300

    massiv

---
+++ script.js
@@ -0,0 +1,35 @@
+// const arr = [1, 2, 3, 4, 5, 12];
+
+// arr.forEach(function (item, i, arr) {
+// console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// function compareNum(a,b){
+
+// }
+
+
+
+
+// arr[99] = 9;
+
+// arr.pop();
+// arr.push(10);
+
+// console.log(arr.length);
+
+
+// for(let i = 0; i < SafeArray.length; i++){
+//     console.log(arr[i]);
+// }
+
+
+// for (let val of arr) {
+// //     console.log(val);
+// }
+
+
+// const str = prompt("", '')
+// const products = str.split(','); 
+// products.sort();
+// console.log(products.join(' ; '));
